001e6ae7a47280d296605766b9fc9320918aa780
gradus <sgrishkevitch@gmail.com>
2012-03-30 17:21:50 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Irish::Application.config.secret_token = 'b29fa0460e10e61d2cbfd990408bfc7839060743398d48feb86b685d5d7caeac678bf1791a1226c3aa91ed492b0ff2216db3c346982292052e9f189a8875bc18'
